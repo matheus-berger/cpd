5a4d90a132838361694e3286bd1acc6828f22e38
Matheus Berger <119708800+matheus-berger@users.noreply.github.com>
2024-11-05 01:26:47 +0000

    On branch algoritimos-pesquisa  Changes to be committed: 	new file:   algoritimos-pesquisa/InterpolacaoPesquisa.py 	modified:   algoritimos-pesquisa/SaltoPesquisa.py

---
+++ algoritimos-pesquisa/InterpolacaoPesquisa.py
@@ -0,0 +1,34 @@
+
+import math
+
+# Função para pesquisa binaria
+def pesquisaBinaria(lista, valor):
+   
+    indice_inicio = 0
+    indice_fim = len(lista)-1
+    indice_meio = math.ceil(indice_inicio + ((indice_fim - indice_inicio) * (valor - lista[indice_inicio])) / (lista[indice_fim] - lista[indice_inicio]))
+    
+    
+    if lista[indice_meio] == valor:
+        print(f"Valor {lista[indice_meio]}, encontrado na posição {indice_meio}!")
+        return 
+    else: 
+        if lista[indice_meio] < valor:
+            sub_lista = lista[indice_meio:indice_fim]
+            pesquisaBinaria(sub_lista, valor)
+        else:
+            sub_lista = lista[indice_inicio:indice_meio]
+            pesquisaBinaria(sub_lista, valor)
+
+
+# Função main
+def main():
+    
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    pesquisaBinaria(lista, 7)
+
+    return True
+
+
+# Chamando a função main
+main()
--- algoritimos-pesquisa/SaltoPesquisa.py
+++ algoritimos-pesquisa/SaltoPesquisa.py
@@ -1,2 +1,38 @@
 
-m = int(math.sqrt(n))
+import math
+
+# Função para pesquisa linear
+def linearPesquisa(lista, valor):
+
+    for i in range(len(lista)):
+        if lista[i] == valor:
+            return lista[i]
+    return False
+
+# Função para pesquisa em salto
+def saltoPesquisa(lista, valor):
+    
+    inicio = lista[0]
+    salto = int(math.sqrt(valor))
+
+    if salto > len(lista):
+        sub_lista = lista[0:]
+        linearPesquisa(sub_lista, valor)
+    else:
+        sub_lista = lista[0:salto]
+        encontrar = linearPesquisa(lista, valor)
+        if encontrar != True:
+            print(f"Valor {encontrar}, encontrado!")
+        else:
+            saltoPesquisa(sub_lista, valor)
+        
+# Funcao main
+def main():
+
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    saltoPesquisa(lista, 7)
+
+
+# Chamando a funcao main
+main()
